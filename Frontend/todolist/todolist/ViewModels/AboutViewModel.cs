a135fd092ac9d288ae7512f36576015b5f072662
Quilv <levanqui88@gmail.com>
2020-08-06 11:37:46 +0700

    + add frontend

---
+++ Frontend/todolist/todolist/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace todolist.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://blog.quilv.com"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
