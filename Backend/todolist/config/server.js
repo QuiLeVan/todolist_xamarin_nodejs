68ae05881d84310fd89ffa8a46ca005040396ee0
Quilv <levanqui88@gmail.com>
2020-08-06 09:22:18 +0700

    + init backend

---
+++ Backend/todolist/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '36523bdd354175b40cd0c326984715b2'),
+    },
+  },
+});
---
+++ Backend/todolist/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ee0ea91c-754c-420a-9151-101f691b1f13'
+};
